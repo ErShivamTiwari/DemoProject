f3be539a20a4505eb0b8e92a3423006fd510cef5
Shivam Tiwari <er.tiwarishivam@gmail.com>
2024-06-09 14:35:57 +0530

    bean scope

--- demoProject/src/main/java/com/demo/demoproject/rest/DemoController.java
+++ demoProject/src/main/java/com/demo/demoproject/rest/DemoController.java
@@ -9,16 +9,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoController {
     private Coach myCoach;
+
+    private Coach myCoach2;
     @Autowired
 
-    DemoController(@Qualifier("baseBallCoach") Coach coach) {
+    DemoController(
+            @Qualifier("baseBallCoach") Coach coach,
+            @Qualifier("baseBallCoach") Coach coach2
+            ) {
         System.out.println("in demoController const "+ getClass().getSimpleName());
         this.myCoach = coach;
+        this.myCoach2 = coach2;
     }
 
     @GetMapping("/Coach")
     public String comment(){
         return myCoach.getDailyWorkout();
+    }
+    @GetMapping("/check")
+    public String check(){
+        return (myCoach==myCoach2)? "Dono same hai":"dono alag hai";
+//        output is No
     }
 }
 
